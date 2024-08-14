5167870c5b96d4aba1eadc4045d6e149fa74d8b4
JacintoErnesto <mendesjacinto8@gmal.com>
2024-08-14 09:54:24 +0100

    feat: add Booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { FootprintsIcon, HandIcon, SearchIcon } from "lucide-react";
+import { SearchIcon } from "lucide-react";
 import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import Image from "next/image";
 import { Input } from "./_components/ui/input";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import { db } from './_lib/prisma'
 import BarbershopItem from "./_components/barbershop- items";
+import { quickSearchOptions } from "./_constants/search";
+import Booking from "./_components/booking-item";
 
 
 
@@ -37,59 +37,18 @@ const Home = async () => {
         {/* Busca rapida */}
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/cabelo.svg"
-              height={16}
-              width={16}
-              alt="Cabelo" />
-            Cabelo
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                height={16}
+                width={16}
+                alt={option.title} />
+              {option.title}
+            </Button>
 
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/barba.svg"
-              height={16}
-              width={16}
-              alt="Barba" />
-            Barba
-          </Button>
+          ))}
 
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg"
-              height={16}
-              width={16}
-              alt="Acabamento" />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/hidratacao.svg"
-              height={16}
-              width={16}
-              alt="Hidração" />
-            Hidração
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/massagem.svg"
-              height={16}
-              width={16}
-              alt="Massagem" />
-            Massagem
-          </Button>
-          <Button className="gap-2" variant="secondary">
-           <FootprintsIcon size={16}/>
-            Unhas dos pés
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-           <HandIcon size={16}/>
-            Unhas das mãos
-          </Button>
         </div>
 
         {/* Banner */}
@@ -101,33 +60,7 @@ const Home = async () => {
             className="object-cover rounded-xl" />
         </div>
         {/* Agendamentos */}
-
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card >
-
-          <CardContent className="flex justify-between p-0">
-            {/* Equerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3>Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p>Barbearia FSW</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center px-5 border-l-2">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">13</p>
-              <p className="text-sm">09:20</p>
-            </div>
-          </CardContent>
-        </Card>
-
+        <Booking/>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
